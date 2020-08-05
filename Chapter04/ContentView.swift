42eec9afbdfb50254c7ceddd78f1a5464ce5f069
jose.marchena <jose.marchena@ocado.com>
2020-08-05 17:15:12 +0200

    Initial Commit

---
+++ Chapter04/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Chapter04
+//
+//  Created by Jose Marchena on 05/08/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
